4ef199e7ea6f0e6d337b23e8ee8d9dd0fb2c93f6
zimacl <andrei.terex89@gmail.com>
2020-12-21 02:02:28 +0200

    created more or less model, that get our choice and redirect to page with results with summ of voutes for each choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
+
+
 
 
 
 
 # Register your models here.
+# Здесь тупим... зарегестрировали только класс (модель) Question, а объявили в models.py,
+# что есть еще и класс (модель) Choice, регестрируем её тоже...
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import Question
 from django.http import Http404
 
-
-#def detail(request, question_id):
-#    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+from .models import Question
+from .models import Choice
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def index(request):
@@ -30,6 +44,13 @@ def detail(request, question_id):
 
 
 
+# И здесь НЕ тупим - импортируем класс/модель Choice а то функцию троит, не видит где класс Choice...
+
+
+
+
+
+
 #def detail(request, question_id):
 #    try:
 #        question = Question.objects.get(pk=question_id)
@@ -39,7 +60,8 @@ def detail(request, question_id):
 
 
 
-
+#def vote(request, question_id):
+#    return HttpResponse("You're voting on question %s." % question_id)
 
 
 
@@ -52,3 +74,9 @@ def detail(request, question_id):
 #    return HttpResponse(template.render(context, request))
 
 
+#def detail(request, question_id):
+#    return HttpResponse("You're looking at question %s." % question_id)
+
+#def results(request, question_id):
+#    response = "You're looking at the results of question %s."
+#    return HttpResponse(response % question_id)
